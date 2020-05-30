cdad6dedaa2f5b7c9276fc61ea945523a0123118
Kazuki Tsujino <hungair0925@gmail.com>
2020-05-30 22:00:26 +0900

    詳細ビュー関数の追記 Part3

--- polls/views.py
+++ polls/views.py
@@ -1,15 +1,26 @@
 from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Question
 
 def index(request):
     template_name = 'polls/index.html'
+
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
+
     return render(request, template_name, context)
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    template_name = 'polls/detail.html'
+
+    try:
+        question = Question.objects.get(pk=question_id)
+        context = {'question': question}
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request, template_name, context)
 
 def results(request, question_id):
     response = f"You're looking at the results of question {question_id}."
